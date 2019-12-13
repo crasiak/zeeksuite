d469d71f52a499d1f1bd513f814b753a52364ecc
Jonathan Stasiak <jw@crasiak.net>
2019-12-12 22:23:55 -0800

    Fixes #15, #13, #6, #1
    startd #10 - needs testing on jenkins
    
    issue #7 requires a binary to built to install on this distro

--- packer/scripts/vagrant.sh
+++ packer/scripts/vagrant.sh
@@ -5,7 +5,7 @@ date > /etc/vagrant_box_build_time
 SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_PASS=${SSH_PASSWORD:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/vagrant}
-
+# private key in zeeksuite/packer
 VAGRANT_INSECURE_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCrtkqkEKKOBT2jHAvFhGf5T2RVF7JZNGKb5zNr3X+i4RNP9KzRRRJsmfWe/NkXwQfm7qfjRueZyTh5NBDGvhCrNNpQngBxe7oanHVCWirCgsMtfm3ltGxtMChd0iecOdGj9FSUWUBW21meF35goBLMxvHdXZOBlKTd/PKFpScvsiIdX5d8w8z0Hmze9cGjTqB7hzkpC4NfuwXM9zekapcVJM9ieergxzu1qp8ydEloI4PR8U4b+rO4lOzVKZUU9+FqEHM60ig0LXg4kNPpxJJ3atiMbnsSmDIbdZIMhc/3aYybQSOE48HkqcnxWJpXI1jOtlNAqQHjMCCygUYrWzIqytA6xiJIoUTjpash57pvZyMAlZKo6QXOWh0sBO9/TgCJ7zHQW8BpjHVyc+3ve2kOchnXohv6C/15k0QcuKwrFs8i0MZ0ReXQzDkcUK/lfCEesBCrs+VnbO2cIZzfmepRDF23RqmQlSPzc9N7fYN4IZcTkhfjtS03h3U104rMZPBuV8AtHcPCXHqlDTR65U91MT9boBd3nltHe3raECMg68Cv7v9hnoP9CExMI+k5I/C17My3GbWoPeIDym5kYx2OXN3R0xVYoY4dVDPF0VvptX1/T19f5r7hJAR+2TmUaDVcJ3NUVSAT13QCXDf0XWRbZHmP8i8AaT96B0MHbeEPkQ=="
 
 
@@ -35,7 +35,6 @@ if [ "$INSTALL_VAGRANT_KEY" = "true" ] || [ "$INSTALL_VAGRANT_KEY" = "1" ]; then
     chmod 700 $SSH_USER_HOME/.ssh
     cd $SSH_USER_HOME/.ssh
 
-    # https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
     echo "${VAGRANT_INSECURE_KEY}" > $SSH_USER_HOME/.ssh/authorized_keys
     chmod 600 $SSH_USER_HOME/.ssh/authorized_keys
     chown -R $SSH_USER:$SSH_USER $SSH_USER_HOME/.ssh
